8d3ec696757dd276de7f61f9daa2e38b1539705f
Xeostop1 <stop1@daum.net>
2022-07-29 13:50:22 +0900

    Add: js파일추가 - 슬라이드 이미지기능 추가

---
+++ src/Script/about.js
@@ -0,0 +1,20 @@
+    window.addEventListener('load',function (){
+
+  $(".sliderWrap").slick({
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      slidesToScroll: 1,
+      autoplay: true,
+      infinite : true, 
+      autoplaySpeed: 5000,
+      pauseOnHover : true,
+      arrows: false
+      // dots : true
+  })
+
+
+
+
+    })
+      
