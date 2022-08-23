e8ab60628da9b1a4310010335afdf93a60d5a4b9
Xeostop1 <stop1@daum.net>
2022-08-23 18:00:33 +0900

    add:js 로컬스토리지에 save 6:32 시청

--- js/todo.js
+++ js/todo.js
@@ -4,6 +4,8 @@ window.addEventListener("load", function(){
     let todoInput=document.querySelector("#todoInput");
     let ul=document.querySelector("ul");
     let liTag=document.querySelector("ul li");
+    let memoArr=[];
+    //노드에 넣을 때 마다 push!로 넣어줌 arrryList.add(data); 처럼
     
 
     //왜 함수를 따로 만드는가?! 여러가지 역할이 들어가게 되면 
@@ -13,8 +15,10 @@ window.addEventListener("load", function(){
     todoForm.addEventListener("submit", function (event) {
         event.preventDefault();      //서브밋을 통해 전달되지 않게 함수처리
         let memo=todoInput.value;
+        memoArr.push(memo);
         todoInput.value="";
         createTag(memo);//위의 벨류값을 통해 넣어줌
+        saveMemo();
     });
 
     //innerHTML 인자를 통해 tag를 넣어줌 
@@ -35,13 +39,25 @@ window.addEventListener("load", function(){
         ul.appendChild(li) //이렇게 연결 해도 들어가지네 우왕!
     }
     
-    function delMemo(){
-        console.log("펑션 만들기 전에 항상 생각하자!");
+    //이벤트가 직접들어간 곳에 target찾아와서 삭제
+    function delMemo(event){
+        //지금은 리턴만 해준상태임 값을 파라미터로 세팅하지 않았음
+        let li= event.target.parentElement;
+        //아 타켓을 가져와야 클릭한 li가 가져올수 있구나
+        //그런데 리무브는 파라미터가 없이도 쓸수 있음??? 
+        //리무브는 요소을 삭제하는거라서 노드 자체를 없애는거고
+        //리무브차일드는 부모에게서 자식 객체를 삭제하는것
+        //그래서 정확하게 프로퍼티에 명시해줘야함
+        li.remove();
+        //버튼 부모의 어떤것이 클릭되었는지 알수 있는것
     }
    
     //인풋은 꼭!!!!! 폼안에 들어가야 잡히네몰랐음;;; 
     //화면 만들때도 꼭 주의, 버튼은 폼 안에 없어도 돔요소가 잡힘
 
-   
+   function saveMemo() {
+        localStorage.setItem("memos", memoArr);//키랑 벨류
+        //로컬스토리지에 세팅하기
+   }
 
 });
